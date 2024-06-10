50eecf59d82a902af6f106d856472d80670709c3
Rafael Kurniawan <rafael.kurniawan@binus.ac.id>
2024-06-10 23:12:54 +0700

    fix AV validation

--- second_pag.js
+++ second_pag.js
@@ -15,18 +15,24 @@ async function calculateFileSHA256(file) {
 document.addEventListener('DOMContentLoaded', function() {
     const fileInput = document.getElementById('fileInput');
     const confirmBtn = document.getElementById('confirmBtn');
-    const loading = document.getElementById('loading');
-    loading.style.display = 'none'; // Initially hidden
+    const loadingText = document.createElement('p');
+    loadingText.id = 'loadingText';
+    loadingText.style.display = 'none';
+    loadingText.innerHTML = 'Scanning...';
+    confirmBtn.insertAdjacentElement('afterend', loadingText);
 
     confirmBtn.addEventListener('click', async function() {
         const file = fileInput.files[0];
         if (file) {
-            loading.style.display = 'block'; // Show loading indicator
+            loadingText.style.display = 'block';
+            document.getElementById("Avira").innerHTML = "";
+            document.getElementById("Kaspersky").innerHTML = "";
+
             const sha256sum = await calculateFileSHA256(file);
             console.log('SHA-256 sum:', sha256sum);
             const form = new FormData();
             form.append('file', file);
-            
+
             // POST SHA256 FILE
             const options1 = {
                 method: 'POST',
@@ -41,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 .then(response => response.json())
                 .then(response => console.log(response))
                 .catch(err => console.error(err));
-            
+
             // GET SHA256 FILE
             setTimeout(() => {
                 // GET API
@@ -50,25 +56,67 @@ document.addEventListener('DOMContentLoaded', function() {
                     .then(response => response.json())
                     .then(response => {
                         console.log(response);
-                        let Avira = response.scans.Avira.detected;
-                        let AviRes = Avira ? `<span style="color: red;"><b>Malware</b></span>` : `<span style="color: green;"><b>Safe</b></span>`;
-                        
-                        let BitDefender = response.scans.BitDefender.detected;
-                        let BitRes = BitDefender ? `<span style="color: red;"><b>Malware</b></span>` : `<span style="color: green;"><b>Safe</b></span>`;
-                        
-                        let Kaspersky = response.scans.Kaspersky.detected;
-                        let KasRes = Kaspersky ? `<span style="color: red;"><b>Malware</b></span>` : `<span style="color: green;"><b>Safe</b></span>`;
-                        
-                        let Sophos = response.scans.Sophos.detected;
-                        let SopRes = Sophos ? `<span style="color: red;"><b>Malware</b></span>` : `<span style="color: green;"><b>Safe</b></span>`;
-                        
+
+                        let AviRes;
+                        if (response.scans.Avira) {
+                            let Avira = response.scans.Avira.detected;
+                            console.log(Avira);
+                            if (Avira == false) {
+                                AviRes = `<span style="color: green;"><b>Safe</b></span>`;
+                            } else if (Avira == true) {
+                                AviRes = `<span style="color: red;"><b>Malware</b></span>`;
+                            }
+                        } else {
+                            AviRes = `<span style="color: grey;"><b>NO DATA</b></span>`;
+                        }
+
+                        let BitRes;
+                        if (response.scans.BitDefender) {
+                            let BitDefender = response.scans.BitDefender.detected;
+                            console.log(BitDefender);
+                            if (BitDefender == false) {
+                                BitRes = `<span style="color: green;"><b>Safe</b></span>`;
+                            } else if (BitDefender == true) {
+                                BitRes = `<span style="color: red;"><b>Malware</b></span>`;
+                            }
+                        } else {
+                            BitRes = `<span style="color: grey;"><b>NO DATA</b></span>`;
+                        }
+
+                        let KasRes;
+                        if (response.scans.Kaspersky) {
+                            let Kaspersky = response.scans.Kaspersky.detected;
+                            console.log(Kaspersky);
+                            if (Kaspersky == false) {
+                                KasRes = `<span style="color: green;"><b>Safe</b></span>`;
+                            } else if (Kaspersky == true) {
+                                KasRes = `<span style="color: red;"><b>Malware</b></span>`;
+                            }
+                        } else {
+                            KasRes = `<span style="color: grey;"><b>NO DATA</b></span>`;
+                        }
+
+                        let SopRes;
+                        if (response.scans.Sophos) {
+                            let Sophos = response.scans.Sophos.detected;
+                            console.log(Sophos);
+                            if (Sophos == false) {
+                                SopRes = `<span style="color: green;"><b>Safe</b></span>`;
+                            } else if (Sophos == true) {
+                                SopRes = `<span style="color: red;"><b>Malware</b></span>`;
+                            }
+                        } else {
+                            SopRes = `<span style="color: grey;"><b>NO DATA</b></span>`;
+                        }
+
                         document.getElementById("Avira").innerHTML = "<b>Avira:</b> " + "<small>" + AviRes.toUpperCase() + "</small>" + "     &nbsp;&nbsp;<b>BitDefender:</b> " + "<small> &nbsp;" + BitRes.toUpperCase() + "</small>";
                         document.getElementById("Kaspersky").innerHTML = "<b> &ensp; Kaspersky:</b> " + "<small>" + KasRes.toUpperCase() + "</small>" + " &ensp; <b>Sophos:</b> " + "<small>" + SopRes.toUpperCase() + "</small>";
-                        loading.style.display = 'none'; // Hide loading indicator
+
+                        loadingText.style.display = 'none';
                     })
                     .catch(err => {
                         console.error(err);
-                        loading.style.display = 'none'; // Hide loading indicator
+                        loadingText.style.display = 'none';
                     });
             }, 60000);
         }
